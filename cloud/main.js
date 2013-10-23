c3fa5fd396e3e3a203ecc42626eb05a088c61192
Administrator <admin@local.host>
2013-10-23 17:32:36 +0800

    new

--- cloud/main.js
+++ cloud/main.js
@@ -2,5 +2,6 @@ require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+
+  response.success("Hello world!"+response+AV.Cloud.define);
 });
