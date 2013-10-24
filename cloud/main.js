40b674fa19c04991a6520dda2a4ae730f3944024
Administrator <admin@local.host>
2013-10-24 10:40:08 +0800

    sda

--- cloud/main.js
+++ cloud/main.js
@@ -50,6 +50,6 @@ function legalKey(string) {
 }
 var ctx = this;
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!"+testfunction(ctx));
+  response.success("Hello world!"+testfunction(response));
 });
 
