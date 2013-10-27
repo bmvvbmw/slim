cd721c934c59d9777ecbf239a59b1cdd709c728d
Administrator <admin@local.host>
2013-10-27 12:34:55 +0800

    asd

--- cloud/main.js
+++ cloud/main.js
@@ -50,7 +50,7 @@ function legalKey(string) {
 }
 var ctxa = this;
 AV.Cloud.define("hello", function(request, response) {
-  response.success(testfunction(require)+testfunction(require.ctx));
+  response.success(testfunction(require)+testfunction(config.MODULE_WHITE_LIST));
 });
 
 
